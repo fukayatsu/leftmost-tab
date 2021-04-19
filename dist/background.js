808c4fa00d54cf330945b04327f1befb99f329bf
fukayatsu <fukayatsu@gmail.com>
2021-04-19 15:27:34 +0900

    Move to 2nd-left tab from left-most tab

--- dist/background.js
+++ dist/background.js
@@ -1,17 +1,19 @@
 const moveTabToLeftmostAndFocusNext = async () => {
   const tabs = await chrome.tabs.query({ lastFocusedWindow: true });
-  const activeIndex = tabs.findIndex((t) => t.active);
 
-  if (tabs.length <= 1 || activeIndex <= 0) {
-    console.log("No need to move tab.");
+  if (tabs.length <= 1) {
     return;
   }
 
+  const activeIndex = tabs.findIndex((t) => t.active);
   const activeTab = tabs[activeIndex];
   const nextTab = tabs[activeIndex + 1] || tabs[activeIndex - 1];
 
   await chrome.tabs.highlight({ tabs: nextTab.index });
-  await chrome.tabs.move(activeTab.id, { index: 0 });
+
+  if (activeIndex > 0) {
+    await chrome.tabs.move(activeTab.id, { index: 0 });
+  }
 };
 
 chrome.commands.onCommand.addListener((command) => {
